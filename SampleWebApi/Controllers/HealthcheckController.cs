1888b3395b8dbf4cb06e432a20ba306c5d82c52d
mysourcecontrol <sri.kammie@hotmail.com>
2015-06-10 09:05:52 -0400

    Initial cloud sample

---
+++ SampleWebApi/Controllers/HealthcheckController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SampleWebApi.Controllers
+{
+    public class HealthcheckController : ApiController
+    {
+        public HttpResponseMessage Get(bool? shutdown = null)
+        {
+            if (shutdown.HasValue && shutdown.Value)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Shutdown Instance");
+            }
+
+            var response = Request.CreateResponse(HttpStatusCode.OK, "Application is running");
+            return response;
+        }
+    }
+}
---
+++ SampleWebApi/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace SampleWebApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            string instanceId = RoleEnvironment.CurrentRoleInstance.Id;
+            int instanceIndex = 0;
+            if (int.TryParse(instanceId.Substring(instanceId.LastIndexOf(".") + 1), out instanceIndex)) // On cloud.
+            {
+                int.TryParse(instanceId.Substring(instanceId.LastIndexOf("_") + 1), out instanceIndex); // On compute emulator.
+            }
+
+            ViewBag.InstanceId = instanceId;
+
+            return View();
+        }
+    }
+}
